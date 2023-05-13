87013a9f79462cd95417402315e922b2c4e957c5
Desenvolvimento <renan.albuquerque@ifto.edu.br>
2023-05-13 10:45:56 -0300

    Aula 05

--- App.tsx
+++ App.tsx
@@ -1,107 +1,144 @@
-import {Text, View, StyleSheet,TextInput, TouchableOpacity} from 'react-native';
-import React, {useId, useState} from 'react';
+import React, {useState} from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
+
+import {TitleEvent} from './src/components/TitleEvent';
+import {Member} from './src/components/Member';
 
 export default function App() {
-  const [ members, setMembers ] = useState<string[]>([]);
-  const [ name, setName ] = useState('');
-  
-  console.log({members});
-  
-
+  const [name, setName] = useState<string>(''); // Nome do participantes
+  const [members, setMembers] = useState<string[]>([]); // Lista de participantes
 
   function handleSubmit() {
-    setMembers(m => [name, ...m]);
+    if (name.trim().length <= 0) {
+      return;
+    }
+
+    if (members.includes(name.trim())) {
+      return Alert.alert(
+        'Ateção',
+        'O participante: ' + name + ' já foi adicionado!',
+      );
+    }
+
+    setMembers(preState => [name, ...preState]);
+
     setName('');
   }
-  
-  function handleRemove(){
-   
-   
+
+  function onMemberDestroi(member: string) {
+    const newMembers = members.filter(m => m !== member);
+
+    setMembers(newMembers);
+  }
+
+  function handleRemoveMember(member: string) {
+    Alert.alert('Remover', 'Remover o ' + member, [
+      {
+        text: 'Sim',
+        isPreferred: true,
+        onPress: () => {
+          onMemberDestroi(member);
+          console.log('removeu');
+        },
+      },
+      {
+        text: 'Não',
+        isPreferred: false,
+      },
+    ]);
   }
 
   return (
     <View style={styled.container}>
-          <Text key={"titulo"} style={styled.pageTitle}>
-            Nome do evento
-          </Text>
-          <Text key={"dataDoEvento"} style={styled.pageDate}>
-            Sexta, 4 de Novembro de 2022.
-          </Text>
-            <View style={{flexDirection: 'row', gap:7, marginTop:24 }}>
-              <TextInput placeholder="Nome do participante" placeholderTextColor="#6B6B6B" style={styled.input} onChangeText={setName} value={name}/>
-              <TouchableOpacity style={styled.addButton} onPress={handleSubmit}>
-              <Text style={{color: '#FDFCFE', fontSize:24, lineHeight:24, fontWeight: '400'}}>+</Text></TouchableOpacity>
-            </View>
-            <Text key={"listaParticipantes"} style={styled.listTitle}>
-            Participantes
-            </Text>
-            
-            {members.map((name, index) => (    
-              <View style={{flexDirection: 'row', marginTop:10 }}>
-                <Text key={name + index} style={styled.card}>
-                  {name}
-                </Text>
-                <TouchableOpacity style={styled.removeButton} onPress={handleRemove}>
-                <Text style={{color: '#FDFCFE', fontSize:24, lineHeight:24, fontWeight: '400'}}>-</Text></TouchableOpacity>
-            </View>
-          ))}      
+      <TitleEvent
+        key={12}
+        subTitle="Sexta, 4 de Junho de 2023."
+        title="Jogos interno IFTO"
+      />
+      <View style={styled.containerInput}>
+        <TextInput
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+          style={styled.input}
+          onChangeText={setName}
+          value={name}
+        />
 
+        <TouchableOpacity style={styled.buttonAdd} onPress={handleSubmit}>
+          <Text style={styled.labelButton}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text key={3} style={styled.titleMembers}>
+        Nome do evento
+      </Text>
+
+      {members.length > 0 ? (
+        members.map((name, index) => (
+          <Member
+            id={name + index}
+            name={name}
+            onRemove={() => handleRemoveMember(name)}
+          />
+        ))
+      ) : (
+        <Text key={4} style={styled.paragraph}>
+          Ninguém chegou no evento ainda? Adicione participantes a sua lista de
+          presença.
+        </Text>
+      )}
     </View>
-    
   );
 }
+
 const styled = StyleSheet.create({
   container: {
-      backgroundColor: '#131016', 
-      flex: 1, 
-      paddingHorizontal: 24, 
-      paddingVertical: 25
+    backgroundColor: '#131016',
+    flex: 1,
+    paddingHorizontal: 24,
+    paddingVertical: 25,
   },
-  pageTitle:{
-    color: '#FDFCFE', 
-      fontSize: 24, 
-      lineHeight:28.13, 
-      fontWeight: '700'
+  titleMembers: {
+    color: '#FDFCFE',
+    fontSize: 20,
+    lineHeight: 23.44,
+    fontWeight: '700',
+    marginTop: 42,
   },
-  listTitle:{
-    color: '#FDFCFE', 
-      fontSize: 20, 
-      lineHeight:23.44, 
-      fontWeight: '700',
-      paddingVertical: 40
+  paragraph: {
+    color: '#FDFCFE',
+    fontSize: 14,
+    lineHeight: 16.41,
+    fontWeight: '400',
+    marginTop: 42,
+    textAlign: 'center',
   },
-  pageDate:{
-    color: '#6B6B6B',
-      fontWeight:'400',
-      fontSize: 16,
-      lineHeight: 18.75
-  },
-  input:{
-    flex:1,
+  input: {
+    flex: 1,
     height: 56,
-    borderRadius:4,
+    borderRadius: 4,
     backgroundColor: '#1F1E25',
     color: '#FDFCFE',
     alignItems: 'center',
     paddingHorizontal: 16,
     fontSize: 14,
     lineHeight: 20,
-    fontWeight: '400'  
+    fontWeight: '400',
   },
-  card:{
-    flex:1,
-    height: 60,
-    borderRadius:4,
-    backgroundColor: '#1F1E25',
-    color: '#FDFCFE',
+  containerInput: {
+    flexDirection: 'row',
     alignItems: 'center',
-    textAlignVertical: 'center',
-    paddingHorizontal: 18,
-    fontSize: 16,
-    lineHeight: 19,
-    fontWeight: '400'  
+    marginTop: 34,
+    gap: 7,
   },
-  addButton:{
+  buttonAdd: {
     alignItems: 'center',
     justifyContent: 'center',
     width: 56,
@@ -109,12 +146,18 @@ const styled = StyleSheet.create({
     borderRadius: 4,
     backgroundColor: '#31CF67',
   },
-  removeButton:{
+  buttonRemove: {
     alignItems: 'center',
     justifyContent: 'center',
     width: 56,
-    height: 60,
+    height: 56,
     borderRadius: 4,
     backgroundColor: '#E23C44',
-  }
-})
+  },
+  labelButton: {
+    color: '#fff',
+    fontSize: 24,
+    lineHeight: 24,
+    fontWeight: '400',
+  },
+});
